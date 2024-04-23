050a01b71339a67920fd5920cdc6d617649f2f33
ripred31 <ripred31@gmail.com>
2024-04-23 18:37:07 -0400

    Stored db creds in env

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -4,11 +4,11 @@ import mysql from 'mysql2/promise';
 export async function GET(req, res) {
 
     const dbconnection = await mysql.createConnection({
-        host: '73.79.171.54',
-        port: '3306',
-        user: 'martin',
-        password: 'SeniorCapstone2024!',
-        database: 'peer-mentor'
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     })
 
     try {
