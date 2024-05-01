da6ea6bd3c14170ab19f1934738aa624f8857384
ripred31 <ripred31@gmail.com>
2024-05-01 08:07:16 -0400

    Sign in page progress plus new queries

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -3,14 +3,14 @@ import Link from 'next/link';
 export default function Navbar() {
     return (
         <nav className="flex h-full">
-            <div className="bg-rose-900 w-48 p-4">
+            <div className="bg-rose-900 w-48 p-4 flex-grow">
                 <Link href="/dashboard">
                     <p className="block py-2 px-4 text-white hover:bg-rose-700 rounded-md">Dashboard</p>
                 </Link>
                 <Link href="/schedule">
                     <p className="block py-2 px-4 text-white hover:bg-rose-700 rounded-md">Schedule</p>
                 </Link>
-                <Link href="/about">
+                <Link href="/">
                     <p className="block py-2 px-4 text-white hover:bg-rose-700 rounded-md">About</p>
                 </Link>
                 <Link href="/sign-out">
--- src/app/sign-in/page.js
+++ src/app/sign-in/page.js
@@ -1,5 +1,37 @@
+import Header from "../components/Header"
+
 export default function SignIn() {
     return(
-        <div>sign in page place</div>
+        <div className="h-full">
+            <Header />
+            <div className="flex items-center justify-center mt-28">
+                <form className="bg-rose-900 text-white p-12 rounded-md">
+                    <p className="font-bold text-xl">Sign In</p>
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-bold mb-2 mt-6">Email</label>
+                        <input
+                            id="email"
+                            type="email"
+                            placeholder="Email"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            required
+                        />
+                    </div>
+                    <div className="my-6">
+                    <label htmlFor="password" className="block text-sm font-bold mb-2">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Password"
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        required
+                    />
+                    </div>
+                    <div className="flex items-center justify-between mt-8">
+                        <button type="submit" className="bg-rose-700 hover:bg-rose-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Sign In</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
