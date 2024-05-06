701708ed961d3b68dc5c422a0dccf0e4f016a550
ripred31 <ripred31@gmail.com>
2024-05-06 19:43:44 -0400

    Added conditional rendering to booking table

--- src/app/components/BookingTable.js
+++ src/app/components/BookingTable.js
@@ -3,19 +3,41 @@ import dayjs from 'dayjs';
 
 export default function BookingTable({ userId, selectedDate }) {
     const [bookings, setBookings] = useState([]);
+    const [userType, setUserType] = useState('');
+
+    useEffect(() => {
+        const fetchUserInfo = async () => {
+            const response = await fetch(`/api/getUserInfo?id=${userId}`);
+            const data = await response.json();
+            setUserType(data.userInfo.UserType);
+        };
+
+        fetchUserInfo();
+    }, [userId]);
 
     useEffect(() => {
         const fetchBookings = async () => {
             const formattedDate = selectedDate ? dayjs(selectedDate).format('YYYY-MM-DD') : '';
             const sqlDate = dayjs(formattedDate).format('YYYY-MM-DD');
-
-            const response = await fetch(`/api/getUserBookings?userId=${userId}&selectedDate=${sqlDate}`);
+            
+            let apiUrl;
+            if (userType === 'Mentee') {
+                apiUrl = `/api/getMenteeBookings?userId=${userId}&selectedDate=${sqlDate}`;
+            } else if (userType === 'Mentor') {
+                apiUrl = `/api/getMentorBookings?userId=${userId}&selectedDate=${sqlDate}`;
+            } else {
+                console.error('Invalid user type');
+                return;
+            }
+    
+            const response = await fetch(apiUrl);
             const data = await response.json();
             setBookings(data.bookings);
         };
-
+    
         fetchBookings();
-    }, [userId, selectedDate]);
+    }, [userId, selectedDate, userType]);
+    
     return (
         <div>
             {bookings.map(booking => (
@@ -29,8 +51,8 @@ export default function BookingTable({ userId, selectedDate }) {
                         <p>{booking.location}</p>
                     </div>
                     <div className='flex'>
-                        <label className='text-md font-bold mr-4'>Mentor:</label>
-                        <p>{booking.mentorName}</p>
+                        <label className='text-md font-bold mr-4'>{userType === 'Mentee' ? 'Mentor' : 'Mentee'}</label>
+                        <p>{userType === 'Mentee' ? booking.mentorName : booking.menteeName}</p>
                     </div>
                     <div className='flex'>
                         <label className='text-md font-bold mr-4'>Status:</label>
