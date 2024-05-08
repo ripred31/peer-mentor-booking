3412a1b38d233d0b720ae52adc449119ed005af5
ripred31 <ripred31@gmail.com>
2024-05-08 16:16:01 -0400

    Angels changes

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,20 +1,21 @@
+import React from 'react';
 import Link from 'next/link';
 
 export default function Navbar() {
     return (
         <div className='flex h-auto'>
-            <div className="bg-rose-900 w-48 p-4 flex-grow flex flex-col justify-between">
+            <div className="bg-rose-900 p-4 flex w-full flex-row justify-center">
                 <div>
                     <Link href="/dashboard">
-                        <p className="block py-2 px-4 text-white hover:bg-rose-700 rounded-md">Dashboard</p>
+                        <p className="inline-block py-2 px-4 text-white hover:bg-rose-700 rounded-md">Dashboard</p>
                     </Link>
                     <Link href="/">
-                        <p className="block py-2 px-4 text-white hover:bg-rose-700 rounded-md">About</p>
+                        <p className="inline-block py-2 px-4 text-white hover:bg-rose-700 rounded-md">About</p>
                     </Link>
                 </div>
                 <div>
                     <Link href="/">
-                        <p className="block py-2 px-4 text-white hover:bg-rose-700 rounded-md">Sign Out</p>
+                        <p className="inline-block py-2 px-4 text-white hover:bg-rose-700 rounded-md">Sign Out</p>
                     </Link>
                 </div>
             </div>
