b5ac3369d36f07c5c726463e316d236457dc10cf
ripred31 <ripred31@gmail.com>
2024-05-08 17:42:07 -0400

    bug fix

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,9 +1,8 @@
-import React from 'react
+import React from 'react'
 import Navbar from "./Navbar"
 
 export default function Header() {
     return(
-        main
         <div>
             <div className="flex justify-center p-12 text-white bg-rose-900 text-4xl font-bold">
                 Peer Mentor Booking Platform
